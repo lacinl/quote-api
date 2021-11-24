17b82da8a058b5abd67f599409c22a2a09edccb0
lacinl <93205493+lacinl@users.noreply.github.com>
2021-11-24 20:37:23 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,17 @@
+const quotes =[
+    { quote:'Be yourself; everyone else is already taken.',
+       person: 'Oscar Wilde'
+    },
+    { quote: 'Two things are infinite: the universe and human stupidity; and I am not sure about the universe.',
+     person: 'Albert Einstein'
+    },
+    { quote:'To live is the rarest thing in the world. Most people exist, that is all.',
+    person: 'Oscar Wilde'
+    },
+    { quote: 'We accept the love we think we deserve.',
+    person: 'Stephen Chbosky'
+    },
+    {quote:'Be the change that you wish to see in the world.',
+    person: 'Mahatma Gandhi'},
+]
+ module.exports = {quotes};
